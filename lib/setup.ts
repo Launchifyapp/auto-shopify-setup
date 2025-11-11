f40ebc59df0715e791c712f604710632961da6d0
Launchifyapp <launchify.business@gmail.com>
2025-11-11 16:49:15 +0100

    Update delivery information in setup function

--- lib/setup.ts
+++ lib/setup.ts
@@ -1,14 +1,14 @@
 export async function runFullSetup({ shop, token }: { shop: string; token: string }) {
   const livraisonHtml = `
-    <p class="p1"><b>Livraison GRATUITE</b><b></b></p>
-    <p class="p1">Le traitement des commandes prend de 1 à 3 jours ouvrables avant l'expédition. Une fois l'article expédié, le délai de livraison estimé est le suivant:</p>
-    <ul class="ul1">
-      <li class="li1">France : 4-10 jours ouvrables</li>
-      <li class="li1">Belgique: 4-10 jours ouvrables</li>
-      <li class="li1">Suisse : 7-12 jours ouvrables</li>
-      <li class="li1">Canada : 7-12 jours ouvrables</li>
-    </ul>
-    <p class="p1">•&nbsp;&nbsp;•&nbsp;&nbsp;Reste du monde : 7-14 jours</p>
+<p class="p1"><b>Livraison GRATUITE</b><b></b></p>
+<p class="p1">Le traitement des commandes prend de 1 à 3 jours ouvrables avant l'expédition. Une fois l'article expédié, le délai de livraison estimé est le suivant:</p>
+<ul class="ul1">
+<li class="li1">France : 4-10 jours ouvrables</li>
+<li class="li1">Belgique: 4-10 jours ouvrables</li>
+<li class="li1">Suisse : 7-12 jours ouvrables</li>
+<li class="li1">Canada : 7-12 jours ouvrables</li>
+<li class="li1">Reste du monde : 7-14 jours</li>
+</ul>
   `.trim();
 
   try {
