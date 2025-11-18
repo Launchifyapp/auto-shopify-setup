a77eacf35a3eff8b227ab0e14cb5b0be17e19d5a
Launchifyapp <launchify.business@gmail.com>
2025-11-18 19:42:23 +0100

    Refactor import statement and format code

--- lib/utils/parseCsvShopify.ts
+++ lib/utils/parseCsvShopify.ts
@@ -1,12 +1,12 @@
-import parse from "csv-parse/lib/sync";
+import { parse } from "csv-parse/lib/sync";
 
 export function parseCsvShopify(csvText: string): any[] {
   return parse(csvText, {
-    delimiter: ";",        // Ton CSV utilise des ";"
-    columns: true,         // Donne un objet par ligne
+    delimiter: ";",
+    columns: true,
     skip_empty_lines: true,
     relax_column_count: true,
-    quote: '"',            // Gère le multi-ligne/HTML/;
-    trim: true             // Nettoie auto espaces
+    quote: '"',
+    trim: true
   });
 }
